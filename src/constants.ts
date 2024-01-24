f722cece9429a2260dcf1498fe92279899733e18
coordova <grcordova@gmail.com>
2024-01-24 18:54:40 -0400

    bt-astro-blog - Pagination Component

--- src/constants.ts
+++ src/constants.ts
@@ -2,4 +2,4 @@ export const SITE_TITLE = "TechPeople Blog";
 export const SITE_DESCRIPTION =
 	"Articles, Stories & Tutorials From Tech People";
 export const HOMEPAGE_ARTICLE_LIMIT = 7;
-export const ARTICLES_PER_PAGE = 6;
+export const ARTICLES_PER_PAGE = 3;
